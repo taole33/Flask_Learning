32ac7dfeede839ee0d1de7b7d4bd23a52c247de9
Shirai <taole33@hotmail.com>
2020-07-08 21:57:48 +0900

    Add files via upload

---
+++ 013uploadfile/uploader.py
@@ -0,0 +1,43 @@
+import os
+
+from flask import Flask,render_template,request,redirect,url_for,send_from_directory,session
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['png','jpg','gif','JPG','jpeg'])
+app.config['UPLOAD_FOLDER']= UPLOAD_FOLDER
+app.config['SECRET_KEY'] = os.urandom(24)
+
+def allowed_file(filename):
+    return '.' in filename and\
+        filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send',methods=['GET','POST'])
+def send():
+    if request.method == 'POST':
+        img_file = request.files['img_file']
+        if img_file and allowed_file(img_file.filename):
+            filename = secure_filename(img_file.filename)
+            img_file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            img_url = '/uploads/' + filename
+            return render_template('index.html',img_url=img_url)
+        else:
+            return '<P>dont allowed</P>'
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+    
